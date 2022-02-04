d1c6bcd3b9d361940c2d492b557ba1492dd808b8
PERSONAL <PERSONAL@PERSONAL-PC>
2022-02-04 12:59:45 +0530

    first commit

---
+++ 03-02-22/src/com/demo/Example2.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+/**
+ * @author PERSONAL
+ *
+ */
+public class Example2 {
+
+	public static void main(String[] args) {
+	int a=20;
+	int b=10;
+	System.out.println(a+b);
+
+	}
+
+}
+
